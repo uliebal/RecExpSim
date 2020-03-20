baf491e424df5ef0362a88f8af9620af49ec648b
Ulf Liebal <ulf.liebal@rwth-aachen.de>
2020-03-20 23:25:58 +0100

    Initial files

---
+++ BioLabSimFun.py
@@ -0,0 +1,9 @@
+# Biotechnology Laboratory Simulator Functions
+
+class Mutant:
+    '''
+    The 'Mutant' class stores all information about the organism.
+    '''   
+    def __init__(self, Host):
+        self.host = Host
+#         self.promoter = None
