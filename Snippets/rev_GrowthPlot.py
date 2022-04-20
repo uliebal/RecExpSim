61bced8bfd3eb0d87ceb51809958590ab17a24e2
Ulf Liebal <ulf.liebal@rwth-aachen.de>
2022-04-20 12:15:56 +0200

    add temperature exp waiting param

--- Snippets/rev_GrowthPlot.py
+++ Snippets/rev_GrowthPlot.py
@@ -1,2 +1,8 @@
 # Insert the correct code sequence for plotting in this cell.
-# %load Snippets/snip_GrowthPlot.py 
+# %load Snippets/rev_GrowthPlot.py 
+Time, Biomass = my_data[:,0], my_data[:,1:]
+DataFile = os.path.join('Data','Growth_Simulation.csv')
+LnBiomass = np.log(Biomass)
+[plt.scatter(Time, X, label=Exp) for Exp,X in enumerate(LnBiomass.T)]
+plt.legend([r'{}:{}$^\circ$C'.format(Idx, T) for Idx, T in enumerate(temperatures)], bbox_to_anchor=(1.05, 1), loc='upper left'); plt.xlabel('time, h'); plt.ylabel('ln(Biomass)')
+my_data = np.genfromtxt(DataFile, delimiter=',', skip_header=1)
