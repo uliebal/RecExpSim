6ad73a9bdf5975b00452ca7d36f9d343612a2a4f
Ulf Liebal <ulf.liebal@rwth-aachen.de>
2022-07-07 15:41:51 +0200

    add python intro exercises, np->1.22

---
+++ Snippets/PythonIntro_Medium.py
@@ -0,0 +1,12 @@
+# enter your code in this cell.
+# Please take your time and try to solve the quest. 
+# If you get stuck for the medium level exercise, uncomment the last comment line to get the solution. 
+# However, the code lines are disordered and you have to fix indentation of for-loop content.
+# %load Snippets/PythonIntro_Medium.py
+
+name_list = name.split()
+print(surname[i])
+surname = name_list[-1]
+myLen = len(surname)
+for i in range(myLen-1,-1,-1):
+name = input('Name: ')
