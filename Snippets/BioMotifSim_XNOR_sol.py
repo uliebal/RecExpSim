184a5b35368f870bad9c374433fd5a0c33e6aa56
Ulf Liebal <ulf.liebal@rwth-aachen.de>
2023-06-23 12:11:00 +0200

    finalize BioMotifSim seminar

---
+++ Snippets/BioMotifSim_XNOR_sol.py
@@ -0,0 +1,38 @@
+# %load Snippets/BioMotifSim_XNOR_ori.py
+# Get x and y values for plotting
+x_log = np.logspace(-2, 2, 200)
+y_log = np.logspace(-2, 2, 200)
+x = np.linspace(0, 2, 200)
+y = np.linspace(0, 2, 200)
+xx, yy = np.meshgrid(x, y)
+xx_log, yy_log = np.meshgrid(x_log, y_log)
+
+# Parameters (steep Hill functions)
+nx = 10
+ny = 10
+
+def myresponse(x, y, nx, ny):
+    # Add equation for the interaction of transcription factors A and B based on the input x and y.
+    # Use the Hill function with OR logic, single occupancy from above.
+    A = biocircuits.aa_and(x, y, nx, ny) # first term of [(A and B) or (not A and not B)]
+    B = biocircuits.rr_and(x, y, nx, ny) # second term of [(A and B) or (not A and not B)]
+    return (A**nx + B**nx) / (1 + A**nx + B**nx)
+
+# Generate plots
+p_and = xyz_im_plot(
+    xx,
+    yy,
+    myresponse(xx, yy, nx, ny),
+    xx_log,
+    yy_log,
+    myresponse(xx_log, yy_log, nx, ny),
+    title="XNOR logic gate",
+)
+
+
+bokeh.io.show(
+    bokeh.layouts.column(
+        bokeh.layouts.row(p_and, bokeh.models.Spacer(width=30)),
+    )
+)
+# %load Snippets/BioMotifSim_XNOR_sol.py
