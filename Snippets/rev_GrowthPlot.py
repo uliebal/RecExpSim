70eabba23a620788e5625b9b7cf44b010870b1ac
Ulf Liebal <ulf.liebal@rwth-aachen.de>
2023-10-05 16:32:22 +0200

    GroExpSim prepare CodeCamp

--- Snippets/rev_GrowthPlot.py
+++ Snippets/rev_GrowthPlot.py
@@ -1,7 +1,8 @@
 # Insert the correct code sequence for plotting in this cell.
-# %load Snippets/rev_GrowthPlot.py 
+# %load ../Snippets/rev_GrowthPlot.py 
+
 Time, Biomass = my_data[:,0], my_data[:,1:]
-DataFile = os.path.join('Data','Growth_Simulation.csv')
+DataFile = os.path.join('..','Data','TempGrowthExperiment.csv')
 LnBiomass = np.log(Biomass)
 [plt.scatter(Time, X, label=Exp) for Exp,X in enumerate(LnBiomass.T)]
 plt.legend([r'{}:{}$^\circ$C'.format(Idx, T) for Idx, T in enumerate(temperatures)], bbox_to_anchor=(1.05, 1), loc='upper left'); plt.xlabel('time, h'); plt.ylabel('ln(Biomass)')
